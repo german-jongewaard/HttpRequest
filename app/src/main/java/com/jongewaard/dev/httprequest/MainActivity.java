4409a8ee163153108fad637b728267137e5ea6c9
german <germanjongewaard@gmail.com>
2018-02-27 02:51:25 +0100

    Implements Json

--- app/src/main/java/com/jongewaard/dev/httprequest/MainActivity.java
+++ app/src/main/java/com/jongewaard/dev/httprequest/MainActivity.java
@@ -10,12 +10,20 @@ import com.google.gson.GsonBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://samples.openweathermap.org/data/2.5/")
+                .build();
+
+        WeatherService service = retrofit.create(WeatherService.class);
 
     }
 }
