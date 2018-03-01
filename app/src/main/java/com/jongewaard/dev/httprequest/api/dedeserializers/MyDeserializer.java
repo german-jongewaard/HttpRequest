bbf9c65cd87ef895bfa9e4dd67d3fa6936a76f86
german <germanjongewaard@gmail.com>
2018-03-01 01:04:50 +0100

    Implements Json

--- app/src/main/java/com/jongewaard/dev/httprequest/api/dedeserializers/MyDeserializer.java
+++ app/src/main/java/com/jongewaard/dev/httprequest/api/dedeserializers/MyDeserializer.java
@@ -1,8 +1,23 @@
 package com.jongewaard.dev.httprequest.api.dedeserializers;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.jongewaard.dev.httprequest.models.City;
+
+import java.lang.reflect.Type;
+
 /**
  * Created by german on 28-2-18.
  */
 
-public class MyDeserializer {
+public class MyDeserializer implements JsonDeserializer<City>{
+    @Override
+    public City deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+
+
+        return null;
+    }
 }
